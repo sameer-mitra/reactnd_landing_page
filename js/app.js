5545deb03bdb9378a201632c08c6b3ec1322318d
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2021-04-16 19:22:58 +1000

    SM 20210416 Added navigation and smooth scrolling

--- js/app.js
+++ js/app.js
@@ -17,6 +17,10 @@
  * Define Global Variables
  * 
 */
+const navbar = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+const navItems = [];
+let navbarHeight = 0;
 
 
 /**
@@ -34,13 +38,43 @@
 */
 
 // build the nav
+function buildNav() {
+    for (section of sections) {
+        const navItem = document.createElement('li');
+        const navLink = document.createElement('a');
+        navLink.textContent = section.querySelector('h2').textContent;
+        navItem.classList.add('nav-item');
+        navLink.classList.add('nav-link');
+        navLink.href = "#" + section.id;
+        navItem.id = "nav-" + section.id;
+        navItem.appendChild(navLink);
+        navbar.appendChild(navItem);
 
+        const navItemTemp = {
+            elementNav: navLink,
+            elementSection: section
+          };
+      
+          navItems.push(navItemTemp);
+    }
+    navbarHeight = document.getElementById('top').offsetHeight;
+}
+
+buildNav();
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
+navbar.addEventListener('click', (event) => {
+    event.preventDefault();
 
+    navItems.forEach(({elementSection, elementNav}) => {
+        if (elementNav === event.target) {
+        elementSection.scrollIntoView({behavior: 'smooth'});
+        }
+    });
+});
 
 /**
  * End Main Functions
