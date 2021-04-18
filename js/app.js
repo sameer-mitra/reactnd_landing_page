c1a213e25c8fbd53694915d222a50140b21edae4
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2021-04-18 12:42:40 +1000

    SM 20210418 Added scrolling feature. Minor CSS changes.

--- js/app.js
+++ js/app.js
@@ -20,10 +20,9 @@
 const navbar = document.getElementById('navbar__list');
 const header = document.querySelector('header');
 const sections = document.querySelectorAll('section');
+const backToTopBtn = document.querySelector('#back-to-top');
 const navItems = [];
 let navbarHeight = 0;
-const backToTopBtn = document.querySelector('#back-to-top');
-
 
 /**
  * End Global Variables
@@ -34,31 +33,37 @@ const backToTopBtn = document.querySelector('#back-to-top');
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    let scrollPosition = window.scrollY;
-    const headerHeight = header.offsetHeight;
+  let scrollPosition = window.scrollY;
+  const headerHeight = header.offsetHeight;
   if (scrollPosition > headerHeight) {
     backToTopBtn.classList.add('visible');
   } else {
     backToTopBtn.classList.remove('visible');
   }
 
-    // Also set the section to active while scrolling.
-    //let bActiveSet = false;
-    //resetActiveState();
+  // Also set the section to active while scrolling.
+  //let bActiveSet = false;
+  resetActiveState(); 
 
-    //navItems.forEach(({elementSection, elementNavLink, elementNavItem}) => {
-    //    if (!bActiveSet && elementSection.top >= scrollPosition) {            
-    //        elementSection.classList.add('active-section');
-    //        elementNavItem.classList.add('active-nav');
-    //        bActiveSet = true;
-    //    }
-    //});
+  navItems.forEach(({elementSection, elementNavLink, elementNavItem}) => {
+    //Add class 'active' to section when near top of viewport
+    let hero_section = document.getElementById('hero');
+    let topHeight = navbarHeight + hero_section.offsetHeight;
+    if (
+      (elementSection.offsetTop - window.pageYOffset < topHeight)
+      &&
+      (elementSection.offsetTop + elementSection.offsetHeight - window.pageYOffset > topHeight)
+    ) {
+      elementNavItem.classList.add('active-nav');
+      elementSection.classList.add('active-section');
+    }
+  });
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = navbarHeight; // For Safari
+  document.documentElement.scrollTop = navbarHeight; // For Chrome, Firefox, IE and Opera
 }
 
 
@@ -70,26 +75,26 @@ function topFunction() {
 
 // build the nav
 function buildNav() {
-    for (section of sections) {
-        const navItem = document.createElement('li');
-        const navLink = document.createElement('a');
-        navLink.textContent = section.querySelector('h2').textContent;
-        navItem.classList.add('nav-item');
-        navLink.classList.add('nav-link');
-        navLink.href = "#" + section.id;
-        navItem.id = "nav-" + section.id;
-        navItem.appendChild(navLink);
-        navbar.appendChild(navItem);
+  for (section of sections) {
+    const navItem = document.createElement('li');
+    const navLink = document.createElement('a');
+    navLink.textContent = section.querySelector('h2').textContent;
+    navItem.classList.add('nav-item');
+    navLink.classList.add('nav-link');
+    navLink.href = "#" + section.id;
+    navItem.id = "nav-" + section.id;
+    navItem.appendChild(navLink);
+    navbar.appendChild(navItem);
 
-        const navItemTemp = {
-            elementNavLink: navLink,
-            elementNavItem: navItem,
-            elementSection: section
-          };
-      
-          navItems.push(navItemTemp);
-    }
-    navbarHeight = document.getElementById('top').offsetHeight;
+    const navItemTemp = {
+      elementNavLink: navLink,
+      elementNavItem: navItem,
+      elementSection: section
+    };
+    
+    navItems.push(navItemTemp);
+  }
+  navbarHeight = document.getElementById('top').offsetHeight;
 }
 
 buildNav();
@@ -99,22 +104,22 @@ buildNav();
 
 // Scroll to anchor ID using scrollTO event
 navbar.addEventListener('click', (event) => {
-    event.preventDefault();
-    resetActiveState();
+  event.preventDefault();
+  resetActiveState();
 
-    navItems.forEach(({elementSection, elementNavLink, elementNavItem}) => {
-        if (elementNavLink === event.target) {
-            elementSection.scrollIntoView({behavior: 'smooth'});
-            elementSection.classList.add('active-section');
-            elementNavItem.classList.add('active-nav');
-        }
-    });
+  navItems.forEach(({elementSection, elementNavLink, elementNavItem}) => {
+    if (elementNavLink === event.target) {
+      elementSection.scrollIntoView({behavior: 'smooth'});
+      elementSection.classList.add('active-section');
+      elementNavItem.classList.add('active-nav');
+    }
+  });
 });
 
 function resetActiveState(){
     navItems.forEach(({elementSection, elementNavLink, elementNavItem}) => {        
-        elementSection.classList.remove('active-section'); 
-        elementNavItem.classList.remove('active-nav');      
+      elementSection.classList.remove('active-section'); 
+      elementNavItem.classList.remove('active-nav');      
     });
 }
 
